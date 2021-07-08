2d501c2da529775f70c911207f52f508ac9e21a3
ZyrnDev <development@zyrn.dev>
2021-07-08 21:43:49 +1000

    Initial Site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+const enviroment = process.env.NODE_ENV || "development";
+const isProduction = enviroment === 'production';
+
+module.exports = (phase, { defaultConfig }) => {
+  
+  const baseConfig = {
+      // Use the prefix in production and not development.
+      assetPrefix: isProduction ? '' : '',
+      ...defaultConfig,
+      poweredByHeader: false,
+      reactStrictMode: true, // Good Practice apparently
+  };
+
+  /* development only config options here */
+  if (phase === PHASE_DEVELOPMENT_SERVER) return { ...baseConfig };
+
+  /* config options for all phases except development here */
+  return { ...baseConfig };
 }
